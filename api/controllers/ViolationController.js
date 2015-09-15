c65b675983c39b53a8e35e65898cc342d4b74e57
G-h-o-s-t <pavel@thankyou.ru>
2015-09-15 11:03:44 +0300

    First commit.

---
+++ api/controllers/ViolationController.js
@@ -0,0 +1,11 @@
+/**
+ * ViolationController
+ *
+ * @description :: Server-side logic for managing Violations
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
